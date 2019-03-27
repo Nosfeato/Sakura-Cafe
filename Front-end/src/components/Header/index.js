764f2c3af5ea708986af86ac5485b6ec647cb8c1
John <kilik.972@gmail.com>
2019-03-27 11:51:11 +0100

    starting toggle button

--- Front-end/src/components/Header/index.js
+++ Front-end/src/components/Header/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /**
  * NPM import
  */
@@ -18,14 +19,22 @@ import './header.scss';
 const Header = () => (
   <header id="header">
     <div id="head">
-      <button id="toggle" type="button">Animation</button>
+      <div className="onoffswitch">
+        <input type="checkbox" name="onoffswitch" className="onoffswitch-checkbox" id="myonoffswitch" checked />
+        <label className="onoffswitch-label" htmlFor="myonoffswitch">
+          <span className="onoffswitch-inner" />
+          <span className="onoffswitch-switch" />
+        </label>
+        <p id="animation__text">Animation</p>
+      </div> { /* End of onoffswitch */ }
       <img src="src/img/logothib2.png" id="logo" alt="sakura-logo" />
+
       <div id="session__info">
-        <button id="sign__in__button"type="button"><a href="/sign-in"> Sign-in </a></button>
+        <button id="sign__in__button" type="button"><a href="/sign-in"> Sign-in </a></button>
         <button id="sign__up__button" type="button"><a href="/sign-up"> Sign-up </a></button>
-        <button id="sign__up__button" type="button"><a href="#"><i class="fa fa-shopping-cart"></i></a></button>
-      </div>
-    </div>
+        <button id="sign__up__button" type="button"><a href="#"><i className="fa fa-shopping-cart" /></a></button>
+      </div> { /* End of session__info */ }
+    </div> { /* End of #head */ }
     <Nav />
   </header>
 );
